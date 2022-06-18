1865c9e76fbd21e866e21ccbb8893b4fe147093c
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-19 01:52:54 +0300

    style:initialize class Profile

--- ujamaa/models.py
+++ ujamaa/models.py
@@ -1,7 +1,8 @@
-from logging.config import _LoggerConfiguration
-from tkinter import CASCADE
-from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django import forms
+
 
 # Create your models here.
 
@@ -13,3 +14,14 @@ class Location(models.Model):
 class Neighborhood(models.Model):
     name = models.CharField(max_length=75)
     location = models.ForeignKey(Location,on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo =models.ImageField(upload_to='images')
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE,null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+    joined=models.DateTimeField(auto_now=True)
+
