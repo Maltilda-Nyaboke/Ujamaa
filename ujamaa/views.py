742a409c7f231d34bd12b7f4cbeb3d9fad5eb840
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-18 23:16:30 +0300

    style:add form elements

--- ujamaa/views.py
+++ ujamaa/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from .
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect
+from .forms import RegisterForm,UpdateProfileForm,AddProjectForm,RatingForm
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import *
 
 # Create your views here.
 
@@ -7,4 +14,11 @@ def home(request):
     return render(request, 'base.html')
 
 def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index.html')
+    else:        
+        form = RegisterForm()
     return render(request, 'register.html')    
