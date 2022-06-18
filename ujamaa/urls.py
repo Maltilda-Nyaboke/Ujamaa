d989176c2b21a8bc73a53ef17a39cf2803735d7b
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-19 01:15:55 +0300

    style:import authenticate properties

--- ujamaa/urls.py
+++ ujamaa/urls.py
@@ -1,5 +1,6 @@
 from . import views
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
 
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/',views.login, name='login'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
