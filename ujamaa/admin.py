8d9097561267ce06e3ec34c4e7cafb9ffbb046d3
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-19 04:39:41 +0300

    style:register models under admin

--- ujamaa/admin.py
+++ ujamaa/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register()
+admin.site.register(Location)
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
