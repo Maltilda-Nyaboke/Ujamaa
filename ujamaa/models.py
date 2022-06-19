de1dff26a8896010b4a575623439909e4f3b0479
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-06-19 05:01:34 +0300

    style:delete profile method

--- ujamaa/models.py
+++ ujamaa/models.py
@@ -64,7 +64,10 @@ class Profile(models.Model):
 
     def save_profile(self):
         self.save()
-
+    
+    def delete_profile(self):
+        self.delete()
+    
     def update(self):
         self.save()    
 
